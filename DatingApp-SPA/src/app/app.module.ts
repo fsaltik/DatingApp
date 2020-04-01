ab7dcdab67aebdf012d84120bed5aff66ce176af
Firat Saltik <f_saltik@yahoo.com>
2020-04-02 00:16:26 +0300

    Routing  & route guard

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { appRoutes } from './routes';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,15 +7,15 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
 
-
-
-
-
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -23,15 +24,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       NavComponent,
       HomeComponent,
       RegisterComponent,
-
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      BrowserAnimationsModule
-   ],
+      BrowserAnimationsModule,
+      RouterModule.forRoot(appRoutes)
+      ],
    providers: [
       AuthService,
       ErrorInterceptorProvider
