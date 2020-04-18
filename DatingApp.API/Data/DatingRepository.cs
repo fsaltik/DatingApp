69b746cb206ce4a8f74dd9a6539fdd22b259f707
Firat Saltik <f_saltik@yahoo.com>
2020-04-18 15:00:47 +0300

    Implementing pagination in the API & Header camelcase propertyname

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -29,11 +30,11 @@ namespace DatingApp.API.Data
             return await _context.SaveChangesAsync() > 0 ;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users =await _context.Users.Include(p => p.Photos)
-                .ToListAsync();
-            return users;
+            var users = _context.Users.Include(p => p.Photos);
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+
         }
 
         public Task<User> GetUser(int id)
